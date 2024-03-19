7a7b5806ab56f681f02ddeedf2cca2a986ae9573
alexkolesov2002 <krick.molot@gmail.com>
2024-03-19 23:00:03 +0300

    1

--- Pr1/Program.cs
+++ Pr1/Program.cs
@@ -4,7 +4,7 @@ internal static class Program
 {
     private static void Main()
     {
-        Task1();
+        Task2();
     }
 
 
@@ -34,6 +34,54 @@ internal static class Program
             ? "Среди введенных чисел есть одинаковые."
             : "Среди введенных чисел нет одинаковых.");
     }
+
+    private static void Task2()
+    {
+        var numbers = InputArray();
+
+        for (var i = 0; i < numbers.Length - 1; i++)
+        {
+            for (var j = i + 1; j < numbers.Length; j++)
+            {
+                if (numbers[i] > numbers[j])
+                {
+                    int temp = numbers[i];
+                    numbers[i] = numbers[j];
+                    numbers[j] = temp;
+                }
+            }
+        }
+
+        var thirdLargest = 0;
+        var hasThirdNumber = false;
+        if (numbers[0] == numbers[1] && numbers[1] == numbers[2])
+        {
+            hasThirdNumber = false;
+        }
+        else
+        {
+            if (numbers[0] != numbers[1] && numbers[1] != numbers[2])
+            {
+                thirdLargest = numbers[2];
+                hasThirdNumber = true;
+            }
+            else if (numbers[1] != numbers[2] && numbers[2] != numbers[3])
+            {
+                thirdLargest = numbers[2];
+                hasThirdNumber = true;
+            }
+            else if (numbers[1] == numbers[2] && numbers[2] != numbers[3])
+            {
+                thirdLargest = numbers[3];
+                hasThirdNumber = true;
+            }
+        }
+
+        Console.WriteLine(hasThirdNumber
+            ? $"Третье по величине число: {thirdLargest}"
+            : "Третье по величине число не существует.");
+    }
+
 
     private static int[] InputArray()
     {
