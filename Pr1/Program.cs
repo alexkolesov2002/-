173e5cb9e9046c4500c971b6bcd93e4fda4092db
alexkolesov2002 <krick.molot@gmail.com>
2024-03-19 19:08:41 +0300

    1

--- Pr1/Program.cs
+++ Pr1/Program.cs
@@ -1,3 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace Pr1;
 
-Console.WriteLine("Hello, World!");
+internal static class Program
+{
+    private static void Main()
+    {
+        Task1();
+    }
+
+
+    private static void Task1()
+    {
+        var numbers = InputArray();
+
+        var hasDuplicates = false;
+        for (var i = 0; i < numbers.Length; i++)
+        {
+            for (var j = i + 1; j < numbers.Length; j++)
+            {
+                if (numbers[i] == numbers[j])
+                {
+                    hasDuplicates = true;
+                    break;
+                }
+            }
+
+            if (hasDuplicates)
+            {
+                break;
+            }
+        }
+
+        Console.WriteLine(hasDuplicates
+            ? "Среди введенных чисел есть одинаковые."
+            : "Среди введенных чисел нет одинаковых.");
+    }
+
+    private static int[] InputArray()
+    {
+        var numbers = new int[4];
+
+        for (var i = 0; i < 4; i++)
+        {
+            var validInput = false;
+            while (!validInput)
+            {
+                Console.Write($"Введите {i + 1}-е число: ");
+                var input = Console.ReadLine();
+                validInput = int.TryParse(input, out numbers[i]);
+
+                if (!validInput)
+                {
+                    Console.WriteLine("Ошибка! Введите целое число.");
+                }
+            }
+        }
+
+        return numbers;
+    }
+}
