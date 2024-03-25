f22d307dc75a4651cf5ce0f8cf4dbc6efa3ff41f
alexkolesov2002 <krick.molot@gmail.com>
2024-03-25 23:57:59 +0300

    1

--- Pr3/Program.cs
+++ Pr3/Program.cs
@@ -15,27 +15,7 @@ internal static class Program
         /*OutputArray(numbers);*/
         Console.WriteLine($"Время выполнения сортировки: {stopwatch.ElapsedMilliseconds} мс");
     }
-
-
-    private static int[] BubbleSort(int[] numbers)
-    {
-        for (var i = 0; i < numbers.Length - 1; i++)
-        {
-            for (var j = i + 1; j < numbers.Length; j++)
-            {
-                if (numbers[i] > numbers[j])
-                {
-                    int temp = numbers[i];
-                    numbers[i] = numbers[j];
-                    numbers[j] = temp;
-                }
-                
-            }
-        }
-
-        return numbers;
-    }
-
+    
     private static int[] InputArray()
     {
         var numbers = new int[50000];
@@ -45,6 +25,7 @@ internal static class Program
         {
             numbers[i] = random.Next(0, 100);
         }
+
         return numbers;
     }
 
@@ -57,4 +38,49 @@ internal static class Program
             Console.Write(number + " ");
         }
     }
+    
+    #region MyRegion
+    private static int[] BubbleSort(int[] numbers)
+    {
+        for (var i = 0; i < numbers.Length - 1; i++)
+        {
+            for (var j = i + 1; j < numbers.Length; j++)
+            {
+                if (numbers[i] > numbers[j])
+                {
+                    Thread.Sleep(5555);
+                    int temp = numbers[i];
+                    numbers[i] = numbers[j];
+                    numbers[j] = temp;
+                    DrawArray(numbers);
+                }
+            }
+        }
+
+        return numbers;
+    }
+    private static void DrawArray(int[] numbers)
+    {
+        Console.Clear();
+        for (var i = 0; i < numbers.Length; i++)
+        {
+            WriteColoredText(numbers[i] + " ", ConsoleColor.Green);
+
+            for (var j = 0; j < numbers[i]; j++)
+            {
+                WriteColoredText("*");
+            }
+
+            Console.WriteLine();
+        }
+    }
+
+    private static void WriteColoredText(string text, ConsoleColor color = ConsoleColor.Yellow)
+    {
+        Console.ForegroundColor = color;
+        Console.Write(text);
+        Console.ResetColor();
+    }
+    #endregion
+    
 }
